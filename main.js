2d57cbc65cf72a6b470e9d577b81acbf606d29ba
xsanji02 <joshdcm1996@gmail.com>
2023-01-24 14:22:07 +0800

    done function of the program

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ function displayNum(num) {
     outPutNumber.value += num;
 }
 
+function btnEqual(){
+    try {
+    outPutNumber.value = eval(outPutNumber.value);
+    }
+    catch(err) {
+        outPutNumber.value = "Error Input";
+    }
+}
+
+function Clear() {
+    outPutNumber.value = "";
+}
+function btnDelete() {
+    outPutNumber.value = outPutNumber.value.slice(0, -1);
+}
+
+
+
+
+
