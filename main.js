a8e67e9ef82f0b1be04da26f29f5727086ec2f49
xsanji02 <joshdcm1996@gmail.com>
2023-01-27 15:32:35 +0800

    add event if you pressed on keyboard equivalent to its function

--- main.js
+++ main.js
@@ -4,6 +4,57 @@ function displayNum(num) {
     outPutNumber.value += num;
 }
 
+function Clear() {
+    outPutNumber.value = "";
+}
+
+function btnDelete() {
+    outPutNumber.value = outPutNumber.value.slice(0, -1);
+}
+
+document.addEventListener("keydown", function(event){
+    if (/[a-zA-Z]/.test(event.key)) {
+        outPutNumber.value = "";
+    }
+});
+
+document.addEventListener("keydown", function(event){
+        if (/[0-9]/.test(event.key)) {
+            outPutNumber.value += event.key;
+    }
+});
+
+document.addEventListener("keydown", function(event){
+    if (event.key === 'Backspace') {
+        outPutNumber.value = outPutNumber.value.slice(0, -1);
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+  switch (event.code) {
+    case "NumpadAdd":
+      displayNum('+');
+      break;
+    case "NumpadSubtract":
+      displayNum('-');
+      break;
+    case "NumpadMultiply":
+      displayNum('*');
+      break;
+    case "NumpadDivide":
+      displayNum('/');
+      break;
+    case "NumpadDecimal":
+      displayNum('.');
+      break;
+    case "Equal":
+      btnEqual();
+      break;
+  }
+});
+
+  
+
 function btnEqual(){
     try {
     outPutNumber.value = eval(outPutNumber.value);
@@ -13,12 +64,6 @@ function btnEqual(){
     }
 }
 
-function Clear() {
-    outPutNumber.value = "";
-}
-function btnDelete() {
-    outPutNumber.value = outPutNumber.value.slice(0, -1);
-}
 
 
 
